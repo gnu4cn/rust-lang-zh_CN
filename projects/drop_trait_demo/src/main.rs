a3d3975782540aff1fbb70c9692e07678c5750c2
Peng Hailin, <unisko@gmail.com>
2023-05-09 21:03:20 +0800

    Update Ch15

--- projects/drop_trait_demo/src/main.rs
+++ projects/drop_trait_demo/src/main.rs
@@ -10,10 +10,10 @@ impl Drop for CustomSmartPointer {
 
 fn main() {
     let c = CustomSmartPointer {
-        data: String::from("c-我的事情"),
+        data: String::from("c - 我的事情"),
     };
     let d = CustomSmartPointer {
-        data: String::from("d-其他事情"),
+        data: String::from("d - 其他事情"),
     };
     println! ("已创建出一些 CustomSmartPointer 实例");
 }
