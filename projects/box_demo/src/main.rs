db7765cab0ae3afc3c6493cc3d346c2126b18448
Unisko PENG <unisko@gmail.com>
2023-05-09 11:26:09 +0800

    Update Ch15

--- projects/box_demo/src/main.rs
+++ projects/box_demo/src/main.rs
@@ -1,3 +1,25 @@
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let boxed_string = MyBox::new(String::from("装箱的字符串"));
+
+    let x = 5;
+    let y = &x;
+    println! ("{}, {}", *boxed_string, *y);
 }
