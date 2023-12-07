af5388f8af07f908d15706f30386bda7d384285a
rust-lang.xfoss.com <unisko@gmail.com>
2023-12-07 15:19:51 +0800

    Refining Ch02.

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -7,23 +7,28 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println! ("秘密数字为：{secret_number}");
+    // println! ("秘密数字为：{secret_number}");
 
-    println! ("请输入你的猜数。");
+    loop {
+        println! ("请输入你的猜数。");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("读取行失败/failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("读取行失败/failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("请输入一个数字！");
+        let guess: u32 = guess.trim().parse().expect("请输入一个数字！");
 
-    println! ("你猜的是：{guess}");
+        println! ("你猜的是：{guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println! ("太小！"),
-        Ordering::Greater => println! ("太大！"),
-        Ordering::Equal => println! ("你赢了！"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println! ("太小！"),
+            Ordering::Greater => println! ("太大！"),
+            Ordering::Equal => {
+                println! ("你赢了！");
+                break;
+            },
+        }
     }
 }
