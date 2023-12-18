2706dfe0c7266f1ac66ce4c913a769a176b594b6
rust-lang.xfoss.com <unisko@gmail.com>
2023-12-18 13:53:02 +0800

    Refining Ch06.

---
+++ projects/match_demo/src/main.rs
@@ -0,0 +1,26 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println! ("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn main() {
+    let penny = Coin::Penny;
+    let dime = Coin::Dime;
+
+    println! ("{}", value_in_cents(penny));
+    println! ("{}", value_in_cents(dime));
+}
