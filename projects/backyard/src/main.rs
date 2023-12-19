cda727fba77f9bb1f4e4e0eb04382d65afb0e22d
Peng Hailin, <unisko@gmail.com>
2023-12-19 22:00:27 +0800

    Fixed a typo-error in 'src/SUMMARY.md'.

--- projects/backyard/src/main.rs
+++ projects/backyard/src/main.rs
@@ -1,3 +1,8 @@
+use crate::garden::vegetables::Asparagus;
+
+pub mod garden;
+
 fn main() {
-    println!("Hello, world!");
+    let plant = Asparagus {};
+    println! ("I'm growing {:?}!", plant);
 }
