20660c4561775cd8d3d0bdc4f0fb72c65f41489d
Hector PENG <unisko@gmail.com>
2025-05-27 15:58:36 +0800

    Finished 'src/async/all_together.md'.

--- projects/hello-async/src/main.rs
+++ projects/hello-async/src/main.rs
@@ -1,60 +1,18 @@
-use std::{pin::pin, time::Duration};
-use trpl::{ReceiverStream, Stream, StreamExt};
-
-fn get_messages() -> impl Stream<Item = String> {
-    let (tx, rx) = trpl::channel();
-
-    trpl::spawn_task(async move {
-        let messages = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
-
-        for (index, message) in messages.into_iter().enumerate() {
-            let time_to_sleep = if index % 2 == 0 { 100 } else { 300 };
-            trpl::sleep(Duration::from_millis(time_to_sleep)).await;
-
-            if let Err(send_error) = tx.send(format!("Message: '{message}'")) {
-                eprintln!("Cannot send message '{message}': {send_error}");
-                break;
-            }
-        }
-    });
-
-    ReceiverStream::new(rx)
-}
-
-fn get_intervals() -> impl Stream<Item = u32> {
-    let (tx, rx) = trpl::channel();
-
-    trpl::spawn_task(async move {
-        let mut count = 0;
-        loop {
-            trpl::sleep(Duration::from_millis(1)).await;
-            count += 1;
-
-            if let Err(send_error) = tx.send(count) {
-                eprintln!("Could not send interval {count}: {send_error}");
-                break;
-            };
-        }
-    });
-
-    ReceiverStream::new(rx)
-}
+use std::{thread, time::Duration};
 
 fn main() {
-    trpl::run(async {
-        let messages = get_messages().timeout(Duration::from_millis(200));
-        let intervals = get_intervals()
-            .map(|count| format!("Interval: {count}"))
-            .throttle(Duration::from_millis(100))
-            .timeout(Duration::from_secs(10));
-        let merged = messages.merge(intervals).take(40);
-        let mut stream = pin!(merged);
+    let (tx, mut rx) = trpl::channel();
 
-        while let Some(result) = stream.next().await {
-            match result {
-                Ok(message) => println!("{message}"),
-                Err(reason) => eprintln!("Problem: {reason:?}"),
-            }
+    thread::spawn(move || {
+        for i in 1..11 {
+            tx.send(i).unwrap();
+            thread::sleep(Duration::from_secs(1));
         }
-    })
+    });
+
+    trpl::run(async {
+        while let Some(message) = rx.recv().await {
+            println!("{message}");
+        }
+    });
 }
--- src/SUMMARY.md
+++ src/SUMMARY.md
@@ -122,6 +122,7 @@
     - [使用任意数量的未来值](async/multiple_futures.md)
     - [流：序列中的未来值](async/streams.md)
     - [近观异步相关的特质](async/async_traits.md)
+    - [放在一起：未来值、任务与线程](async/all_together.md)
 
 
 - [Rust 的面向对象编程特性](Ch17_Object_Oriented_Programming_Features_of_Rust.md)
