228baef40cb00506294af16ac61a47ce853cc1b7
Hector PENG <unisko@gmail.com>
2025-06-06 16:04:21 +0800

    Updated 'src/common_collections/vectors.md'.

--- projects/vec_demo/src/main.rs
+++ projects/vec_demo/src/main.rs
@@ -1,26 +1,8 @@
 fn main() {
-    let mut v = vec! [100, 32, 57];
 
-    for i in &mut v {
-        *i += 50;
-        println! ("{}", i);
-    }
+    let mut v = vec! [1, 2, 3, 4];
 
-    #[derive(Debug)]
-    enum SpreadsheetCell {
-        Int(i32),
-        Float(f64),
-        Text(String),
-    }
+    let last = v.pop().unwrap();
 
-    let row = vec! [
-        SpreadsheetCell::Int(3),
-        SpreadsheetCell::Text(String::from("blue")),
-        SpreadsheetCell::Float(10.12),
-    ];
-
-
-    println! ("{:#?}", &row[0])
-
-    // dbg! (v);
+    println!("{last}, {:?}", v);
 }
