f82d8f36984cf34e06732cca7b7115e0caec10f3
Hector PENG <unisko@gmail.com>
2026-02-05 13:05:45 +0800

    Updated projects/data_types'.

--- projects/data_types/src/main.rs
+++ projects/data_types/src/main.rs
@@ -2,6 +2,8 @@ fn main() {
     let c = 'z';
     let z: char = 'ℤ'; // 带有显式的类型注解
     let heart_eyed_cat = '😻';
+    let a: [i32; 5] = [-1, 0, 1, 2, 3];
 
     println! ("c 为 {c}, z 为 {z}, 爱心猫{heart_eyed_cat}");
+    println! ("a 为 {:#?}", a);
 }
