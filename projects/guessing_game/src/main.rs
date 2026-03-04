b65e63a5018801ee3dd12f4982fcfdc3c61708f1
Hector PENG <unisko@gmail.com>
2026-03-04 18:34:47 +0800

    Updated src/Ch02_Programming_a_Guessing_Game.md'.

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,14 +1,20 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println! ("猜猜这个数!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println! ("秘密数字是：{secret_number}");
+
     println! ("请输入你的猜数。");
 
     let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
-        .expect("读取行失败");
+        .expect("读取行失败！");
 
     println! ("你猜的是: {guess}");
 }
