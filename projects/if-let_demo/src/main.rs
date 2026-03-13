c5794c25bdf8b487ae0a26fc5faed09c14773a80
Hector PENG <unisko@gmail.com>
2026-03-13 19:15:28 +0800

    Updated src/enums_and_pattern_matching/if-let_control_flow.md'.

---
+++ projects/if-let_demo/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let config_max = Some(3u8);
+
+    match config_max {
+        Some(max) => println! ("The maximum is configured to be {max}"),
+        _ => ()
+    }
+
+    if let Some(max) = config_max {
+        println! ("The maximum is configured to be {max}");
+    }
+
+}
