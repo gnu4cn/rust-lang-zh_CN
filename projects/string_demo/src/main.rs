a970f56628d033685780aceee90332f198b1c21d
Hector PENG <unisko@gmail.com>
2026-03-17 13:45:08 +0800

    Updated src/common_collections/string.md'.

--- projects/string_demo/src/main.rs
+++ projects/string_demo/src/main.rs
@@ -1,10 +1,5 @@
 fn main() {
-    let data = "初始内容";
-
-    let s = data.to_string();
-
-    // 该方法直接在字面值之上也工作
-    let s = "初始内容".to_string();
-
-    println! ("{s}");
+    for b in "Зд".bytes() {
+        println!("{b}");
+    }
 }
