92b8750bd4f2c595cb4d666831a512c4c38c0a19
Hector PENG <unisko@gmail.com>
2026-03-29 16:24:21 +0800

    Updated 'src/io_project/std_err.md'.

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println! ("解析参数时遇到问题：{err}");
+        eprintln! ("解析参数时遇到问题：{err}");
         process::exit(1);
     });
 
@@ -14,7 +14,7 @@ fn main() {
         检索 {}", config.file_path, config.query);
 
     if let Err(e) = run(config) {
-        println! ("应用程序错误：{e}");
+        eprintln! ("应用程序错误：{e}");
         process::exit(1);
     }
 }
