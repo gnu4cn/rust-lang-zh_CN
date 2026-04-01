af619f161cfc57cf11a4f2984ae1d56c171fe751
Hector PENG <unisko@gmail.com>
2026-04-01 13:26:23 +0800

    Updated 'src/crates-io/publishing.md'.

--- projects/art/src/lib.rs
+++ projects/art/src/lib.rs
@@ -1,10 +1,36 @@
-//! # art
+//! # 美术
 //!
-//! 建模诸多美术概念的一个库。
-
-pub mod kinds;
-pub mod utils;
+//! 用于建模美术概念的库。
 
 pub use self::kinds::PrimaryColor;
 pub use self::kinds::SecondaryColor;
 pub use self::utils::mix;
+
+
+pub mod kinds {
+    /// 根据 RYB 颜色模型的原色。
+    pub enum PrimaryColor {
+        Red,
+        Yellow,
+        Blue,
+    }
+
+    /// 根据 RYB 颜色模型的间色。
+    #[derive(Debug)]
+    pub enum SecondaryColor {
+        Orange,
+        Green,
+        Purple,
+    }
+}
+
+pub mod utils {
+    use crate::kinds::*;
+
+    /// 等量组合两种原色以创建
+    /// 一种间色。
+    pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
+        // --跳过代码--
+        SecondaryColor::Purple
+    }
+}
