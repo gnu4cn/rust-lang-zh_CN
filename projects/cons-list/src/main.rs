c97c752bcd274b2e29fe0cb7e965ae3f4614f10d
Hector PENG <unisko@gmail.com>
2026-04-04 10:28:27 +0800

    Updated 'src/smart_pointers/rc-t'.

--- projects/cons-list/src/main.rs
+++ projects/cons-list/src/main.rs
@@ -1,4 +1,3 @@
-#[derive(Debug)]
 enum List {
     Cons(i32, Box<List>),
     Nil,
@@ -7,5 +6,7 @@ enum List {
 use crate::List::{Cons, Nil};
 
 fn main() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
+    let b = Cons(3, Box::new(a));
+    let c = Cons(4, Box::new(a));
 }
--- projects/rc_demo/src/main.rs
+++ projects/rc_demo/src/main.rs
@@ -10,8 +10,13 @@ use crate::List::{Cons, Nil};
 
 fn main() {
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println! ("创建 a 后的计数 = {}", Rc::strong_count(&a));
     let b = Cons(3, Rc::clone(&a));
-    let c = Cons(4, Rc::clone(&a));
+    println! ("创建 b 后的计数 = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println! ("创建 c 后的计数 = {}", Rc::strong_count(&a));
+    }
 
-    println! ("b 为: {:?}\nc 为：{:?}", b, c);
+    println! ("c 超出作用域后的计数 = {}", Rc::strong_count(&a));
 }
