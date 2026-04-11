d50c9be81cd7c274264e3b8cb9d343e9210e90a2
Hector PENG <unisko@gmail.com>
2026-04-11 17:13:41 +0800

    Updated 'src/async/concurrency_n_async.md'.

--- projects/message-passing_n_tasks/src/main.rs
+++ projects/message-passing_n_tasks/src/main.rs
@@ -4,21 +4,42 @@ fn main() {
     let fut = async {
         let (tx, mut rx) = trpl::channel();
 
-        let vals = vec! [
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("future"),
-        ];
+        let tx1 = tx.clone();
+        let tx1_fut = async move {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("future"),
+            ];
 
-        for val in vals {
-            tx.send(val).unwrap();
-            trpl::sleep(Duration::from_millis(500)).await;
-        }
+            for val in vals {
+                tx1.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
 
-        while let Some(value) = rx.recv().await {
-            println!("收到 '{value}'");
-        }
+        let rx_fut = async {
+            while let Some(value) = rx.recv().await {
+                println!("收到 '{value}'");
+            }
+        };
+
+        let tx_fut = async move {
+            let vals = vec![
+                String::from("more"),
+                String::from("messages"),
+                String::from("for"),
+                String::from("you"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(1500)).await;
+            }
+        };
+
+        trpl::join! (tx1_fut, tx_fut, rx_fut);
     };
 
     trpl::block_on(fut);
