302a83589161046e64248fd4599196c8d261a88c
Hector PENG <unisko@gmail.com>
2026-04-14 16:01:57 +0800

    Updated 'src/async/async_traits.md'.

--- projects/message-passing_n_tasks/src/main.rs
+++ projects/message-passing_n_tasks/src/main.rs
@@ -1,11 +1,12 @@
 use std::time::Duration;
+use std::pin::{Pin, pin};
 
 fn main() {
     let fut = async {
         let (tx, mut rx) = trpl::channel();
 
         let tx1 = tx.clone();
-        let tx1_fut = async move {
+        let tx1_fut = pin! (async move {
             let vals = vec![
                 String::from("hi"),
                 String::from("from"),
@@ -17,15 +18,15 @@ fn main() {
                 tx1.send(val).unwrap();
                 trpl::sleep(Duration::from_millis(500)).await;
             }
-        };
+        });
 
-        let rx_fut = async {
+        let rx_fut = pin! (async {
             while let Some(value) = rx.recv().await {
                 println!("收到 '{value}'");
             }
-        };
+        });
 
-        let tx_fut = async move {
+        let tx_fut = pin! (async move {
             let vals = vec![
                 String::from("more"),
                 String::from("messages"),
@@ -37,9 +38,12 @@ fn main() {
                 tx.send(val).unwrap();
                 trpl::sleep(Duration::from_millis(1500)).await;
             }
-        };
+        });
 
-        trpl::join! (tx1_fut, tx_fut, rx_fut);
+        let futures: Vec<Pin<&mut dyn Future<Output = ()>>> =
+            vec![tx1_fut, rx_fut, tx_fut];
+
+        trpl::join_all(futures).await;
     };
 
     trpl::block_on(fut);
