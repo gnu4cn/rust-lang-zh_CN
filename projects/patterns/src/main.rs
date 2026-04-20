7cb8292442ef06666a7d9fdd485a41515baa0c9d
Hector PENG <unisko@gmail.com>
2026-04-20 17:11:44 +0800

    Updated 'src/patterns/syntax.md'.

--- projects/patterns/src/main.rs
+++ projects/patterns/src/main.rs
@@ -1,7 +1,25 @@
-fn main() {
-    let x = 5 else {
-        return;
-    };
+enum Color {
+    Rgb(u32, u32, u32),
+    Hsv(u32, u32, u32),
+}
 
-    println! ("{x}");
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(Color),
+}
+
+fn main() {
+    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+
+    match msg {
+        Message::ChangeColor(Color::Rgb(r, g, b)) => {
+            println! ("将颜色改为红 {r}、绿 {g} 及蓝 {b}");
+        }
+        Message::ChangeColor(Color::Hsv(h, s, v)) => {
+            println! ("将颜色改为色调 {h}、饱和度 {s} 及颜色值 {v}");
+        }
+        _ => (),
+    }
 }
