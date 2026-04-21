640e80b0d702af7d799dfeb17b9b1b75a812579c
Hector PENG <unisko@gmail.com>
2026-04-21 16:04:26 +0800

    Updated 'src/patterns/syntax.md'.

--- projects/patterns/src/main.rs
+++ projects/patterns/src/main.rs
@@ -1,25 +1,17 @@
-enum Color {
-    Rgb(u32, u32, u32),
-    Hsv(u32, u32, u32),
-}
-
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(Color),
-}
-
 fn main() {
-    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+    enum Message {
+        Hello { id: u32 },
+    }
+
+    let msg = Message::Hello { id: 5 };
 
     match msg {
-        Message::ChangeColor(Color::Rgb(r, g, b)) => {
-            println! ("将颜色改为红 {r}、绿 {g} 及蓝 {b}");
+        Message::Hello { id: id @ 3..=7 } => {
+            println! ("找到位于范围内的 id: {id}")
         }
-        Message::ChangeColor(Color::Hsv(h, s, v)) => {
-            println! ("将颜色改为色调 {h}、饱和度 {s} 及颜色值 {v}");
+        Message::Hello { id: 10..=12 } => {
+            println! ("找到位于另一范围内 id")
         }
-        _ => (),
+        Message::Hello { id } => println! ("找到别的 id: {id}"),
     }
 }
